9a772770d21b3d56b56219d8dd52f1aae541fbbf
krisnas9792 <krisnas9792@gmail.com>
2026-01-06 16:31:16 +0000

    changing theme and adding font family

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <p className="read-the-docs">
-        Starting to build new application for jns fitness
-      </p>
-    </>
+    <div className="app-container">
+        <div className="card">
+            <h1>JNS Fitness</h1>
+            <p className="read-the-docs">
+                Starting to build the new application for JNS Fitness.
+            </p>
+            <button>Get Started</button>
+        </div>
+    </div>
   )
 }
 
